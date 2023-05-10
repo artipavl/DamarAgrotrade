8932526d0a7995f5d122610a3aad9baebbc4d741
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-05-10 22:17:03 +0300

    update Box

--- src/components/Box/box.style.ts
+++ src/components/Box/box.style.ts
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 
-export const Content = styled.div`
+type ContentProps = {
+  bgc?: string;
+};
+
+export const Content = styled.div<ContentProps>`
   max-width: 1140px;
 
   padding-left: 15px;
   padding-right: 15px;
   padding-top: 0px;
   padding-bottom: 0px;
+  background-color: ${props => props.bgc};
 
   @media (min-width: 1140px) {
     margin: auto;
--- src/components/Box/box.tsx
+++ src/components/Box/box.tsx
@@ -3,11 +3,11 @@ import { Content } from './box.style';
 
 export interface BoxProps {
   children: React.ReactElement;
+  bgc?: string;
 }
 
-function Box({ children }: BoxProps) {
-
-  return <Content>{children}</Content>;
+function Box({ children, bgc }: BoxProps) {
+  return <Content bgc={bgc}>{children}</Content>;
 }
 
 export default Box;
