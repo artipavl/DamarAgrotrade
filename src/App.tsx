32ee20eb4fc6d673c8cbaba678a5b3da80c904fd
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-05-15 15:46:01 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Header from './components/header/header';
 import Home from './pages/home/home';
+import Footer from './components/footer/footer';
+import styled from 'styled-components';
+
+export const AppBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
 
 function App() {
   return (
-    <>
+    <AppBox>
       <Header />
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/*" element={<Navigate to="/"/>} />
-    </Routes>
-    </>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
+      <Footer />
+    </AppBox>
   );
 }
 
