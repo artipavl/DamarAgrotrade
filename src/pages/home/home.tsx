8f5dc3cffc649107134ff86402c9784a2c0c9042
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-05-18 15:39:07 +0300

    Update home.tsx

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Hero from '../../components/hero/hero';
+import About from '../../components/about/about';
 
 function Home() {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }
