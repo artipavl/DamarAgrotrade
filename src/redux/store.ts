5df83c3a35ea1f9ef67b5db7fd26ce2883488701
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-06-20 19:26:11 +0300

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 import basketSlice from './basket/basketSlice';
+import likeSlice from './like/likeSlice';
+import comparisonSlice from './comparison/comparisonSlice';
 
 const store = configureStore({
   reducer: {
     basket: basketSlice,
+    like: likeSlice,
+    comparison: comparisonSlice,
   },
 });
 
